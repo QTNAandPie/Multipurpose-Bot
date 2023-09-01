4d40ffa1d914b9d498c123de3e60bd9b1c57a30d
RainbowCreamPie <quan7colorful@outlook.com>
2023-09-01 22:19:39 +0700

    Connected to MongoDB

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 
 const { Client, GatewayIntentBits } = require('discord.js');
 const { CommandKit } = require('commandkit');
+const mongoose = require('mongoose')
 const path = require('path');
 
 const client = new Client({
@@ -17,5 +18,15 @@ new CommandKit({
     eventsPath: path.join(__dirname, 'events'),
     commandsPath: path.join(__dirname, 'commands'),
 });
+
+(async () => {
+    try {
+        mongoose.set('strictQuery', false),
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('MongoDB | Status : Connected');
+    } catch (error) {
+        console.log(`Error: ${error}`);
+    }
+})();
 
 client.login(process.env.TOKEN);
